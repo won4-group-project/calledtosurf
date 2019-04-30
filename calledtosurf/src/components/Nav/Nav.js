df17d608a429f0dab1ca5eae6fe008bcadffb4ce
Christian's Mac <macbook@Macbooks-MacBook-Pro.local>
2019-04-29 18:41:21 -0600

    Added a components folder

---
+++ calledtosurf/src/components/Nav/Nav.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import  './'
+
+
+class Nav extends Component{
+
+
+    render(){
+        return(
+            <section>
+                <section>
+                    Hello Navigation!
+                </section>
+            </section>
+        )
+    }
+}
+
+export default Nav
