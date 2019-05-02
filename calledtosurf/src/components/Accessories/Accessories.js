968c1b21179bebe7eca1c91f0a71de3bf1d82e0c
divya <divu333@gmail.com>
2019-05-02 11:46:22 -0600

    Added Accessories page

---
+++ calledtosurf/src/components/Accessories/Accessories.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import axios from "axios";
+class Accessories extends Component {
+  constructor() {
+    super();
+    this.state = {
+      accessories: []
+    };
+    this.getAccessoryList = this.getAccessoryList.bind(this);
+  }
+
+  componentDidMount() {
+    this.getAccessoryList();
+  }
+
+  getAccessoryList() {
+    axios.get("/api/shoes").then(res => {
+      console.log("shoesssss" + res.data);
+
+      this.setState({ accessories: res.data });
+    });
+  }
+  render() {
+    let list = this.state.accessories.map(accessory => (
+      <ul key={accessory.product_id}>
+        <li>
+          {accessory.title} {accessory.category}
+        </li>
+      </ul>
+    ));
+
+    return <div> {list}</div>;
+  }
+}
+
+export default Accessories;
