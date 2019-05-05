f97d7b8910052793292ed2309f3e1747971ef209
divya <divu333@gmail.com>
2019-05-05 15:29:59 -0600

    shoe page last update

--- calledtosurf/src/components/Shoes/Shoes.js
+++ calledtosurf/src/components/Shoes/Shoes.js
@@ -1,7 +1,12 @@
 import React, { Component } from "react";
 import axios from "axios";
-import { Card, Container } from "react-bootstrap";
+import { Card, Container, Row, CardDeck, Col } from "react-bootstrap";
 import "../Shoes/shoes.css";
+import boots from "../../assets/Shoes/Boots/58a.jpg";
+import heels from "../../assets/Shoes/Heels/61a.jpg";
+import sandals from "../../assets/Shoes/Sandals/75b.jpg";
+import sneakers from "../../assets/Shoes/Sneakers/77a.jpg";
+import filter from "../../assets/Icons/filter.svg";
 class Shoes extends Component {
   constructor() {
     super();
@@ -24,25 +29,56 @@ class Shoes extends Component {
   }
   render() {
     return (
+      // <Container className="shoe_container">
+      //   {this.state.shoes.map(shoe => (
+      //     <Card
+      //       border="default"
+      //       style={{ width: "13rem" }}
+      //       id={shoe.product_id}
+      //     >
+      //       <Card.Body>
+      //         <div className="zoom">
+      //           <img
+      //             alt="poster"
+      //             width="170"
+      //             src="https://cdn.shopify.com/s/files/1/3000/9920/products/cts-shoes-159_180x.jpg"
+      //             // className="img_container"
+      //           />
+      //         </div>
+      //       </Card.Body>
+      //     </Card>
+      //   ))}
+      // </Container>
       <Container className="shoe_container">
-        {this.state.shoes.map(shoe => (
-          <Card
-            border="default"
-            style={{ width: "13rem" }}
-            id={shoe.product_id}
-          >
-            <Card.Body>
-              <div className="zoom">
-                <img
-                  alt="poster"
-                  width="170"
-                  src="https://cdn.shopify.com/s/files/1/3000/9920/products/cts-shoes-159_180x.jpg"
-                  className="img_container"
-                />
-              </div>
-            </Card.Body>
-          </Card>
-        ))}
+        <Row>
+          <Col className="zoom">
+            <a href="" className="collection_item">
+              <img alt="poster" src={boots} />
+              <span className="collection_title"> Boots</span>
+            </a>
+          </Col>
+          <Col className="zoom">
+            <a href="" className="collection_item">
+              <img alt="poster" src={heels} />
+              <span className="collection_title"> Heels</span>
+            </a>
+          </Col>
+          <Col className="zoom">
+            <a href="" className="collection_item">
+              <img alt="poster" src={sandals} />
+              <span className="collection_title"> Sandals</span>
+            </a>
+          </Col>
+          <Col className="zoom">
+            <a href="" className="collection_item">
+              <img alt="poster" src={sneakers} />
+              <span className="collection_title"> Sneakers</span>
+            </a>
+          </Col>
+        </Row>
+        <div className="collection-filter">
+          <div className="filter_item">products</div>
+        </div>
       </Container>
     );
   }
