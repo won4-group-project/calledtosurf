063046bcee4fe6f9479fa0b82086e607c0cab2ed
divya <divu333@gmail.com>
2019-05-06 11:04:41 -0600

    chnages to accessories and shoe page and css

--- calledtosurf/src/routes.js
+++ calledtosurf/src/routes.js
@@ -3,11 +3,13 @@ import { Switch, Route } from "react-router-dom";
 import Shoes from "./components/Shoes/Shoes";
 import CreateAcct from "./components/UserAccess/CreateAcct/CreateAcct";
 import Login from "./components/UserAccess/Login/Login";
+import Accessories from "./components/Accessories/Accessories";
 export default (
   <Switch>
     <Route component={CreateAcct} exact path="/createaccount" />
     <Route component={Login} exact path="/login" />
 
     <Route component={Shoes} path="/shoes" />
+    <Route component={Accessories} path="/accessories" />
   </Switch>
 );
