c181b0319da294ef3e4f70db65057a5af7ee849e
Christian's Mac <macbook@Macbooks-MacBook-Pro.local>
2019-05-11 06:52:01 -0600

    fixes to server file

--- calledtosurf/server/index.js
+++ calledtosurf/server/index.js
@@ -1,48 +1,26 @@
 require("dotenv").config();
 const express = require("express");
-const session = require('express-session');
 const bodyParser = require("body-parser");
 const massive = require("massive");
-const passport = require('passport');
-const Auth0Strategy = require('passport-auth0');
-const LocalStrategy = require('passport-local');
 const app = express();
 const controller = require("../server/controller");
-const auth = require('./controllers/auth');
+const Auth = require("./controllers/auth");
 // const cors = require("cors");
+app.use(bodyParser.json());
 // app.use(cors());
 
-// ***** App Setup ***** //
-app.use(bodyParser.json());
-app.use(session({
-  secret: process.env.SECRET,
-  resave: false,
-  saveUnitialized: true
-}));
-// ***** Server Setup ***** //
 massive(process.env.CONNECTION_STRING)
-  .then(db => {
-    app.set("db", db);
+  .then(dbInstance => {
+    app.set("db", dbInstance);
   })
   .catch(err => console.log(err));
 
-// ***** Product Routes ***** //
 app.get("/api/shoes", controller.getAll);
 
-// ***** Authentication Routes ***** //
-passport.use('local', new LocalStrategy(
-  function(username, password, done) {
-    db.users.findOne({ username: username }, function (err, user) {
-      if (err) { return done(err); }
-      if (!user) { return done(null, false); }
-      if (!user.verifyPassword(password)) { return done(null, false); }
-      return done(null, user);
-    });
-  }
-));
-app.get('/login', auth.login);
-app.post('/signup',auth.signup);
+// ***** Authentication ***** //
 
+app.get("/login/", Auth.login);
+app.post("/signup", Auth.signup);
 
 // const port = process.env.SERVER_PORT;
 const port = 4000;
