31eb59d6ef65678af1845b466cba71027d0fc2b6
Christian's Mac <macbook@Macbooks-MacBook-Pro.local>
2019-05-13 14:33:39 -0600

    Changed to lowercase pintrest.png, instagram.png

--- calledtosurf/src/routes.js
+++ calledtosurf/src/routes.js
@@ -13,9 +13,9 @@ import Details from "./components/Details/Details";
 export default (
   <Switch>
     <Route component={Home} exact path="/" />
-    <Route component={CreateAcct} exact path="/createaccount" />
-    <Route component={Login} exact path="/login" />
-    <Route component={Account} exact path="/account" />
+    <Route component={CreateAcct} path="/createaccount" />
+    <Route component={Login} path="/login" />
+    <Route component={Account} path="/account" />
     <Route component={Clothing} exact path="/clothing" />
     <Route component={Shoes} path="/shoes" />
     <Route component={Accessories} path="/accessories" />
